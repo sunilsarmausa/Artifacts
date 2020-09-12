0a24dd0ac11bc61199e99cc0df8d1c05719577c4
sunilsarmausa <45651017+sunilsarmausa@users.noreply.github.com>
2020-09-11 23:58:51 -0700

    LT_1

---
+++ Artifacts/LeetCode/LT_0001.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class LT_0001
+    {
+        public static void TestCases()
+        {
+            var arr = new int[] { 2, 7, 11, 15 };
+            Assert.AreEqual(TwoSum(arr, 9), new int[] { 0, 1 });
+
+            arr = new int[] { 3, 2, 4 };
+            Assert.AreEqual(TwoSum(arr, 6), new int[] { 1, 2 });
+
+            arr = new int[] { 3, 3 };
+            Assert.AreEqual(TwoSum(arr, 6), new int[] { 0, 1 });
+        }
+        private static int[] TwoSum(int[] nums, int target)
+        {
+            var store = new Dictionary<int, int>();
+            for (int i = 0, l = nums.Length; i < l; i++)
+            {
+                var reminder = target - nums[i];
+                if (store.ContainsKey(reminder))
+                {
+                    return new int[2] { store[reminder], i };
+                }
+                store[nums[i]] = i;
+            }
+            return new int[2] { -1, -1 };
+        }
+    }
+}
---
+++ Artifacts/LeetCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Sunil Sarma's github LeetCode repository in C#");
+
+
+            LT_0001.TestCases();
+        }
+    }
+}
